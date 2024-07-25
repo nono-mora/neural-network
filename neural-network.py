8fee88337b445475dc2b7376929713ee51b8ddb9
nono-mora <amorab@ucenfotec.ac.cr>
2024-07-25 05:15:13 -0600

    updated weights variables to be in a single list.

--- neural-network.py
+++ neural-network.py
@@ -4,29 +4,29 @@ inputs = [(0.0000, 0.0000), (0.1600, 0.1556), (0.2400, 0.3543), (0.2800, 0.3709)
 targets = [230, 555, 815, 860, 1140, 1085, 1200, 1330, 1290, 870, 1545, 1480, 1750, 1845, 1790, 1955]
 
 
-w1 = 0.1 # Weight for the first input feature (age)
-w2 = 0.2 # Weight for the second input feature (mileage)
-b = 0.3 # "b" is our bias term, allowing the model to fit the data more flexibly 
+weights = [0.0, 0.0] # 
+b = 0.0 # "b" is our bias term, allowing the model to fit the data more flexibly 
 epochs = 7000 #The number of times we will train our neural network.
 learning_rate = 0.1 
 
-def predict(i1, i2): #Predicts the output of the neural network.
-    return w1 * i1 + w2 * i2 + b
+def predict(inputs): #Predicts the output of the neural network.
+    return sum([w * i for w, i in zip(weights, inputs)]) + b 
 
 #Train the network
 for epoch in range(epochs):
-    pred = [predict(i1, i2) for i1, i2 in inputs] #Calculates a prediction for al the inputs. 
+    pred = [predict(inp) for inp in inputs] #Calculates a prediction for al the inputs. 
     cost = sum([(p - t) ** 2 for p, t in zip(pred, targets)]) / len(targets) #Calculates the cost.
     print(f"Epoch: {epoch + 1}, Cost: {cost:.2f}")
     
     #Backpropagation 
     errors_derivative = [2 * (p - t) for p, t in zip(pred, targets)] #Calculates the derivative of the errors.
-    weight1_delta = [e * i[0] for e, i in zip(errors_derivative, inputs)] #Calculates weight1 delta.
-    weight2_delta = [e * i[1] for e, i in zip(errors_derivative, inputs)] #Calculates weight2 delta.
+    weights_delta = [[e * i for i in inp] for e, inp in zip(errors_derivative, inputs)] #Calculates weights delta.
     bias_delta = [e * 1 for e in errors_derivative] #Calculates bias delta.
+    weights_delta_T = list(zip(*weights_delta)) #Transposes the weights delta.
+    
+    for i in range(len(weights)): #Updates the weights.
+        weights[i] -= learning_rate * sum(weights_delta_T[i]) / len(weights_delta)
 
-    w1 -= learning_rate * sum(weight1_delta) / len(weight1_delta) #Updates weight1. 
-    w2 -= learning_rate * sum(weight2_delta) / len(weight2_delta) #Updates weight2. 
     b -= learning_rate * sum(bias_delta) / len(bias_delta) #Updates the bias. 
 
 #print(f"\nw1: {w1:.2f}, w2: {w2:.2f}, b: {b:.2f}")
@@ -35,6 +35,6 @@ for epoch in range(epochs):
 test_inputs = [(0.1600, 0.1391), (0.5600, 0.3046), (0.7600, 0.8013), (0.9600, 0.3046), (0.1600, 0.7185)]
 test_targets = [500, 850, 1650, 950, 1375]
 
-pred = [predict(i1, i2) for i1, i2 in test_inputs]
+pred = [predict(inp) for inp in test_inputs]
 for p, t in zip(pred, test_targets):
     print(f"Prediction: {p:.0f}, Target: {t}")
